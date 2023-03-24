6c5f3317e1262ad24efa831290f0168f884be04e
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2023-03-24 15:32:51 +0300

    PieChart - Campaign and Target Chart

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,7 +1,8 @@
 import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
 import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
-import { useTheme } from "@mui/material";
+import { useTheme, Box, Typography } from "@mui/material";
 import React, { useMemo } from "react";
 import {
   ResponsiveContainer,
@@ -16,28 +17,42 @@ import {
   Legend,
   BarChart,
   Bar,
+  Pie,
+  Cell,
+  PieChart,
 } from "recharts";
 
-type Props = {};
+const pieData = [
+  {
+    name: "Group A",
+    value: 600,
+  },
+  {
+    name: "Group B",
+    value: 400,
+  },
+];
 
-const Row2 = (props: Props) => {
+const Row2 = () => {
   const { palette } = useTheme();
-  const {data :operationalData} = useGetKpisQuery();
-  const { data : productData} = useGetProductsQuery();
+  const pieColors = [palette.primary[800], palette.primary[300]];
+  const { data: operationalData } = useGetKpisQuery();
+  const { data: productData } = useGetProductsQuery();
 
   const operationalExpenses = useMemo(() => {
     return (
       operationalData &&
-      operationalData[0].monthlyData.map(({ month, operationalExpenses, nonOperationalExpenses }) => {
-        return {
-          name: month.substring(0, 3),
-          "Operational Expenses":operationalExpenses,
-          "Non Operational Expenses":nonOperationalExpenses
-        };
-      })
+      operationalData[0].monthlyData.map(
+        ({ month, operationalExpenses, nonOperationalExpenses }) => {
+          return {
+            name: month.substring(0, 3),
+            "Operational Expenses": operationalExpenses,
+            "Non Operational Expenses": nonOperationalExpenses,
+          };
+        }
+      )
     );
   }, [operationalData]);
-
 
   return (
     <>
@@ -92,7 +107,53 @@ const Row2 = (props: Props) => {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="e">
+        <BoxHeader title="Campaigns and Targets" sideText="+4%" />
+        <FlexBetween mt="0.25rem" gap="1.rem" pr="1rem">
+          <PieChart
+            width={110}
+            height={100}
+            margin={{
+              top: 0,
+              right: -10,
+              left: 10,
+              bottom: 0,
+            }}
+          >
+            <Pie
+              stroke="none"
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={pieColors[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+            <Typography variant="h5">Target Sales</Typography>
+            <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>
+              83
+            </Typography>
+            <Typography variant="h6">
+              Finance goals of the campaign that is desired{" "}
+            </Typography>
+          </Box>
+          <Box flexBasis="40%">
+            <Typography variant="h5">Losses in Revenue</Typography>
+            <Typography variant="h6">Losses are down 25%</Typography>
+            <Typography mt="0.4rem" variant="h5">
+              Profit Margins{" "}
+            </Typography>
+            <Typography variant="h6">
+              Margins are up by 30% from last month.{" "}
+            </Typography>
+          </Box>
+        </FlexBetween>
+      </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
   );
