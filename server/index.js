60b8b45626470016c06456263318641247d78e6c
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2023-03-24 16:04:48 +0300

    seed transaction data to database

--- server/index.js
+++ server/index.js
@@ -47,5 +47,6 @@ mongoose
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
     //  Product.insertMany(products);
+    Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connected`));
