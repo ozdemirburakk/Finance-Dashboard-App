b79c0460f1e4e5637f96c4ca52d4b0a1792a9b9c
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2023-03-24 12:25:32 +0300

    revenue and expenses charts

---
+++ client/src/state/types.ts
@@ -0,0 +1,33 @@
+export interface IExpensesByCategory {
+  salaries: number;
+  supplies: number;
+  services: number;
+}
+
+export interface IMonth {
+  id: string;
+  month: string;
+  revenue: number;
+  expenses: number;
+  nonOperationalExpenses: number;
+  operationalExpenses: number;
+}
+
+export interface IDay {
+    id: string;
+    date: string;
+    revenue: number;
+    expenses: number;
+  }
+
+export interface IGetKpisResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  totalProfit: number;
+  totalRevenue: number;
+  totalExpenses: number;
+  expensesByCategory: IExpensesByCategory;
+  monthlyData: Array<IMonth>;
+  dailyData:Array<IDay>;
+}
